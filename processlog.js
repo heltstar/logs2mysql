8ceb995875b7098bdef7a4162d32a0f07b45814a
heltstar <heltstar@gmail.com>
2014-06-26 11:25:26 +0800

    rename two files

--- processlog.js
+++ processlog.js
@@ -1,13 +1,13 @@
-var init_json  = require('./init_json.js');
+var initjson  = require('./initjson.js');
 var write2mysql = require('./write2mysql.js');
-var getfromlogs = require('./get_from_logs.js');
+var getfromlogs = require('./getfromlogs.js');
 var rmcoldfiles = require('./rmcoldfiles.js');
 
 var global = require("./global.js");
 var path =".";
 
 
-init_json.init_json(path);
+initjson.init_json(path);
 
 setTimeout(getfromlogs.getfromlogs,1000);
 
--- rmcoldfiles.js
+++ rmcoldfiles.js
@@ -18,7 +18,6 @@ function rmcoldfiles(){
 
     connection.query('SELECT * from ' + global.CDN_FILE_LFU_STATS + ' where id = 1', function(err, rows, fields) {
         if (err) throw err;
-//        console.log("rows.length:"+rows.length);
         for(var i = 0; i < rows.length; i++)
         {   
             file_path_record = rows[i].file_path;
@@ -27,16 +26,12 @@ function rmcoldfiles(){
                 continue;
             }
             file_path = global.ROOT_PATH + rows[i].file_path;
-//            console.log(i + '---' + file_path_record);
             fs.unlink(file_path, function(err){
                 if(err) console.log("remove file error");
-//                console.log("remove " + file_path + " success");
                 connection.query('DELETE from ' + global.CDN_FILE_LFU_STATS + ' where file_path="' + file_path_record +'"', function(err) {
                     if (err) throw err;
-//                    console.log("delete mysql record from " + TABLE + " where file_path=" + file_path_record + " success");
                     connection.query('DELETE from ' + global.CDN_FILE_RECORD + ' where file_path="' + file_path_record +'"', function(err) {
                         if (err) throw err;
-//                        console.log("delete mysql record from " + CDN_FILE_RECORD + " where file_path=" + file_path_record + " success");
                     });
                 }); 
 
